ed4d045ee4c721a33ad0a13844b37d0a9ff4f700
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-09-06 20:53:20 +0800

    update 'src_path' of create-html.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/create-html.sh
+++ src/blog-web/create-html.sh
@@ -1,14 +1,14 @@
-src_path=/home/sonvhi/chenxiaosong/code # 替换为你的仓库路径
+src_path=/home/sonvhi/chenxiaosong/code/blog # 替换为你的仓库路径
 dst_path=/var/www
 tmp_html_path=${dst_path}/html-tmp
 html_path=${dst_path}/html
 
-. ${src_path}/blog/src/blog-web/common-lib.sh
-. ${src_path}/blog/src/blog-web/array.sh
+. ${src_path}/src/blog-web/common-lib.sh
+. ${src_path}/src/blog-web/array.sh
 
 init_begin() {
     mkdir -p ${tmp_html_path}
-    bash ${src_path}/blog/courses/courses.sh
+    bash ${src_path}/courses/courses.sh
 }
 
 init_end() {
@@ -18,17 +18,17 @@ init_end() {
 
 copy_secret_repository() {
     # pictures是我的私有仓库
-    cp ${src_path}/pictures/public/ ${tmp_html_path}/pictures -rf
+    cp ${src_path}/../pictures/public/ ${tmp_html_path}/pictures -rf
 }
 
 copy_public_files() {
     # css样式
-    cp ${src_path}/blog/src/blog-web/stylesheet.css ${tmp_html_path}/
+    cp ${src_path}/src/blog-web/stylesheet.css ${tmp_html_path}/
 }
 
 init_begin
-create_sign ${src_path}/blog/src/blog-web/sign.md ${tmp_html_path}
-create_html ${src_path}/blog ${tmp_html_path}
+create_sign ${src_path}/src/blog-web/sign.md ${tmp_html_path}
+create_html ${src_path} ${tmp_html_path}
 copy_secret_repository
 copy_public_files
 change_perm ${tmp_html_path}
