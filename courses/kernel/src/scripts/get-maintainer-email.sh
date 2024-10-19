9f7ccdcd9e6e77e6a1fb6c890f37b6095a9d64cd
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-10-19 14:47:24 +0800

    update get-maintainer-email.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- courses/kernel/src/scripts/get-maintainer-email.sh
+++ courses/kernel/src/scripts/get-maintainer-email.sh
@@ -6,6 +6,8 @@
 to_types=('maintainer' 'reviewer' 'supporter' 'commit_signer')
 cc_types=('open list' 'moderated list')
 
+except_emails=('samba-technical@lists.samba.org' 'chenxiaosong@kylinos.cn' 'chenxiaosong@chenxiaosong.com')
+
 to_emails=()
 cc_emails=()
 unknown_emails=()
@@ -41,25 +43,45 @@ is_email_exist() {
 	return 1
 }
 
-# 0: 成功添加到邮件数组中或已存在某个数组中，1: 未添加到数组中
-iter_types() {
+# 0: 是例外的邮箱，1: 不是例外的邮箱
+is_except_email() {
+	local email=$1
+	is_exist except_emails ${email}
+	if [[ $? == 0 ]]; then
+		return 0
+	fi
+	return 1
+}
+
+# 0:
+#   - 成功添加到邮件数组中
+#   - 已存在某个数组中
+#   - 例外的邮箱
+# 1: 未添加到数组中
+add_to_array() {
 	local -n types_array=$1
 	local -n emails_array=$2
+	local str=$3
 
 	local emails_array_name=$2
-	local str=$3
 	# echo ${types_array[@]}
 	# echo ${str}
+
 	local email=$(echo ${str} | awk '{print $1}') # 提取邮箱
+	is_except_email ${email}
+	if [[ $? == 0 ]]; then
+		return 0
+	fi
+
 	for type_name in "${types_array[@]}"; do
-		echo ${str} | grep -E ${type_name} > /dev/null 2>&1
+		echo ${str} | grep -E "${type_name}" > /dev/null 2>&1
 		if [[ $? == 0 ]]; then
 			is_email_exist ${email}
 			if [[ $? == 0 ]]; then
 				echo "${email} already exist"
 				return 0
 			fi
-			echo "${type_name}: ${email}"
+			# echo "${type_name}: ${email}"
 			emails_array+=(${email})
 			# echo "${emails_array_name}: ${emails_array[@]}"
 			return 0
@@ -72,22 +94,26 @@ iter_types() {
 parse_emails() {
 	local str=$1
 	local email=$(echo ${str} | awk '{print $1}') # 提取邮箱
-	iter_types to_types to_emails "${str}"
-	if [[ $? != 0 ]]; then
-		echo "unknown: ${email}"
-		unknown_emails+=(${email})
+	add_to_array to_types to_emails "${str}"
+	if [[ $? == 0 ]]; then
+		return
 	fi
+	add_to_array cc_types cc_emails "${str}"
+	if [[ $? == 0 ]]; then
+		return
+	fi
+	# echo "unknown: ${email}"
+	unknown_emails+=(${email})
 }
 
 iter_str() {
 	local output_str=$1
 	while IFS= read -r line; do
 		local line=$(echo ${line} | sed 's/.* <//') # ' <'之前的部分删除
-		line=$(echo ${line} | sed 's/>//g') # 删除'>'字符
+		line=$(echo ${line} | sed 's/> / /g') # 删除'>'字符
 		# echo "line: ${line}"
 		parse_emails "${line}"
 	done <<< "${output_str}"
-	echo "debug to_emails: ${to_emails[@]}"
 }
 
 parse_pattern() {
@@ -105,15 +131,37 @@ parse_pattern() {
 	done
 }
 
+print_result() {
+	local emails_str="--to="
+	for email in "${to_emails[@]}"; do
+		emails_str+="${email},"
+	done
+	emails_str="${emails_str:0:${#emails_str}-1}" # 去掉最后的逗号
+
+	emails_str+=" --cc="
+	for email in "${cc_emails[@]}"; do
+		emails_str+="${email},"
+	done
+	emails_str="${emails_str:0:${#emails_str}-1}" # 去掉最后的逗号
+
+	echo "git send-email ${emails_str}"
+
+	local unknown_str="unknown emails:"
+	for email in "${unknown_emails[@]}"; do
+		unknown_str+=" ${email}"
+	done
+	echo "${unknown_str}"
+}
+
 test_str=$(cat <<EOF
-Steve French <sfrench@samba.org> (supporter:COMMON INTERNET FILE SYSTEM CLIENT (CIFS and SMB3))
-Paulo Alcantara <pc@manguebit.com> (reviewer:COMMON INTERNET FILE SYSTEM CLIENT (CIFS and SMB3))
-Ronnie Sahlberg <ronniesahlberg@gmail.com> (reviewer:COMMON INTERNET FILE SYSTEM CLIENT (CIFS and SMB3))
-Shyam Prasad N <sprasad@microsoft.com> (reviewer:COMMON INTERNET FILE SYSTEM CLIENT (CIFS and SMB3))
-Tom Talpey <tom@talpey.com> (reviewer:COMMON INTERNET FILE SYSTEM CLIENT (CIFS and SMB3))
-Bharath SM <bharathsm@microsoft.com> (reviewer:COMMON INTERNET FILE SYSTEM CLIENT (CIFS and SMB3))
+this is name <1@test.com> (maintainer:MODULE ONE (A and B))
+this is name <2@test.com> (supporter:MODULE ONE (A and B))
+this is name <2@test.com> (open list:MODULE ONE (A and B))
+this is name <3@test.com> (reviewer:MODULE ONE (A and B))
 linux-cifs@vger.kernel.org (open list:COMMON INTERNET FILE SYSTEM CLIENT (CIFS and SMB3))
 samba-technical@lists.samba.org (moderated list:COMMON INTERNET FILE SYSTEM CLIENT (CIFS and SMB3))
+linux-nfs@vger.kernel.org (open list:NFS, SUNRPC, AND LOCKD CLIENTS)
+linux-kernel@vger.kernel.org (open list)
 linux-kernel@vger.kernel.org (open list)
 EOF
 )
@@ -125,8 +173,6 @@ test() {
 }
 
 pattern=$1
-# parse_pattern "$pattern"
-test
-
-echo "git send-email --to=${to_emails[@]}"
-
+parse_pattern "$pattern"
+# test
+print_result
