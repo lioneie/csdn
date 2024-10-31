lioneie/csdn

commit 1e4f74c9c6d1324888dd993a4a4fa117cf5e844b

remove 'replace_with_public_ip_or_delete_this_line'
Fixes: f700b74 ("局域网时签名中有主页网址")
Fixes: 9c8a787 ("update create-csdn-src.sh")
Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

src/blog-web/common-lib.sh
@@ -3,20 +3,34 @@
 # --toc: 这个选项指示 pandoc 生成一个包含文档目录（Table of Contents，目录）的 HTML 输出。TOC 将包括文档中的章节和子章节的链接，以帮助读者导航文档。
 pandoc_common_options="--to html --standalone --metadata encoding=gbk --number-sections --css https://chenxiaosong.com/stylesheet.css"
 
+replace_with_lan_ip() {
+    dst_file=$1
+    lan_ip=$2
+
+    sed -i 's/chenxiaosong.com/'${lan_ip}'/g' ${dst_file}
+    # 局域网用http，不用https
+    sed -i 's/https:\/\/'${lan_ip}'/http:\/\/'${lan_ip}'/g' ${dst_file}
+    # 邮箱替换回来
+    sed -i 's/@'${lan_ip}'/@chenxiaosong.com/g' ${dst_file}
+}
+
 create_sign() {
     local src_file=$1
     local tmp_html_path=$2
     local is_public_ip=$3
+    local lan_ip=$4
 
     local html_title="签名"
     local dst_file=${tmp_html_path}/sign.html
     local from_format="--from markdown"
     pandoc ${src_file} -o ${dst_file} --metadata title="${html_title}" ${from_format} ${pandoc_common_options}
     # 先去除sign.html文件中其他内容
-    sed -i '/<\/header>/,/<\/body>/!d' ${tmp_html_path}/sign.html # 只保留</header>到</body>的内容
-    sed -i '1d;$d' ${tmp_html_path}/sign.html # 删除第一行和最后一行
-    if [[ ${is_public_ip} == true ]]; then
-        sed -i '/replace_with_public_ip_or_delete_this_line/d' ${tmp_html_path}/sign.html
+    sed -i '/<\/header>/,/<\/body>/!d' ${dst_file} # 只保留</header>到</body>的内容
+    sed -i '1d;$d' ${dst_file} # 删除第一行和最后一行
+    replace_with_lan_ip ${dst_file} ${lan_ip}
+    if [[ ${is_public_ip} == false ]]; then
+        # 在<ul>之后插入公网主页
+        sed -i -e '/<ul>/a<li><a href="https://chenxiaosong.com/">公网主页: chenxiaosong.com</a></li>' ${dst_file}
     fi
 }
 
@@ -32,10 +46,11 @@ create_sign() {
 #        4. 绝对路径的目录，代表源文件路径前缀，这时目的文件和上面的情况3一样
 #    网页标题
 create_html() {
-    # local array=("${!1}") # 使用间接引用来接收数组，调用的地方 create_html array[@] ${src_path} ${tmp_html_path}
+    # local array=("${!1}") # 使用间接引用来接收数组，调用的地方 create_html array[@] ...
     local src_path=$1
     local tmp_html_path=$2
     local sign_path=$3
+    local lan_ip=$4
 
     local element_count="${#array[@]}" # 总个数
     local count_per_line=5
@@ -74,6 +89,7 @@ create_html() {
         fi
         echo "create ${ofile}"
         pandoc ${src_file} -o ${dst_file} --metadata title="${html_title}" ${from_format} ${pandoc_options}
+        replace_with_lan_ip ${dst_file} ${lan_ip}
         if [[ ${is_sign} == 1 ]]; then
             # 在<header之后插入sign.html整个文件
             sed -i -e '/<header/r '${sign_path}'/sign.html' ${dst_file}

src/blog-web/create-html.sh
@@ -5,6 +5,7 @@ html_path=${dst_path}/html
 sign_path=${tmp_html_path}
 
 is_public_ip=$1
+lan_ip=$2
 
 . ${src_path}/src/blog-web/common-lib.sh
 . ${src_path}/src/blog-web/array.sh
@@ -31,8 +32,8 @@ copy_public_files() {
 }
 
 init_begin
-create_sign ${src_path}/src/blog-web/sign.md ${tmp_html_path} ${is_public_ip}
-create_html ${src_path} ${tmp_html_path} ${sign_path}
+create_sign ${src_path}/src/blog-web/sign.md ${tmp_html_path} ${is_public_ip} ${lan_ip}
+create_html ${src_path} ${tmp_html_path} ${sign_path} ${lan_ip}
 copy_secret_repository
 copy_public_files
 change_perm ${tmp_html_path}

src/blog-web/create-csdn-src.sh
@@ -41,7 +41,7 @@ __create_csdn_src() {
 
     echo '[建议点击这里查看个人主页上的最新原文](https://chenxiaosong.com/'${ofile}')' >> ${dst_file}
     echo >> ${dst_file}
-    cat ${src_path}/src/blog-web/sign.md | grep -v replace_with_public_ip_or_delete_this_line >> ${dst_file}
+    cat ${src_path}/src/blog-web/sign.md >> ${dst_file}
     echo >> ${dst_file}
     cat ${src_file} >> ${dst_file}
 

src/blog-web/do-restart.sh
@@ -22,17 +22,10 @@ copy_config() {
     fi
 }
 
-replace_lan_ip() {
+restart_private() {
     # 部署在局域网
     if [ ${is_public_ip} = false ]; then
-        bash ${src_path}/private-blog/scripts/create-html.sh
-        find ${dst_path}/ -type f -exec sed -i 's/chenxiaosong.com/'${lan_ip}'/g' {} +
-        # 局域网用http，不用https
-        find ${dst_path}/ -type f -exec sed -i 's/https:\/\/'${lan_ip}'/http:\/\/'${lan_ip}'/g' {} +
-        # 邮箱替换回来
-        find ${dst_path}/ -type f -exec sed -i 's/@'${lan_ip}'/@chenxiaosong.com/g' {} +
-        # 局域网时签名中有公网主页网址
-        find ${dst_path}/ -type f -exec sed -i 's/replace_with_public_ip_or_delete_this_line/chenxiaosong.com/g' {} +
+        bash ${src_path}/private-blog/scripts/create-html.sh ${lan_ip}
     fi
 }
 
@@ -42,8 +35,8 @@ restart_all() {
     fi
     echo "recreate html, restart service"
     copy_config
-    bash ${src_path}/blog/src/blog-web/create-html.sh ${is_public_ip}
-    replace_lan_ip
+    bash ${src_path}/blog/src/blog-web/create-html.sh ${is_public_ip} ${lan_ip}
+    restart_private
     iptables -F # 根据情况决定是否要清空防火墙规则
     service nginx restart # 重启nginx服务，docker中不支持systemd
 }

commit a5868dfc69aeb595bc3e85e7784283cabf60c6c4

公网不替换成局域网ip
Fixes: 1e4f74c ("remove 'replace_with_public_ip_or_delete_this_line'")
Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

src/blog-web/common-lib.sh
@@ -27,8 +27,9 @@ create_sign() {
     # 先去除sign.html文件中其他内容
     sed -i '/<\/header>/,/<\/body>/!d' ${dst_file} # 只保留</header>到</body>的内容
     sed -i '1d;$d' ${dst_file} # 删除第一行和最后一行
-    replace_with_lan_ip ${dst_file} ${lan_ip}
+    # 局域网的处理
     if [[ ${is_public_ip} == false ]]; then
+        replace_with_lan_ip ${dst_file} ${lan_ip}
         # 在<ul>之后插入公网主页
         sed -i -e '/<ul>/a<li><a href="https://chenxiaosong.com/">公网主页: chenxiaosong.com</a></li>' ${dst_file}
     fi
@@ -50,7 +51,8 @@ create_html() {
     local src_path=$1
     local tmp_html_path=$2
     local sign_path=$3
-    local lan_ip=$4
+    local is_public_ip=$4
+    local lan_ip=$5
 
     local element_count="${#array[@]}" # 总个数
     local count_per_line=5
@@ -89,7 +91,10 @@ create_html() {
         fi
         echo "create ${ofile}"
         pandoc ${src_file} -o ${dst_file} --metadata title="${html_title}" ${from_format} ${pandoc_options}
-        replace_with_lan_ip ${dst_file} ${lan_ip}
+        # 局域网的处理
+        if [[ ${is_public_ip} == false ]]; then
+            replace_with_lan_ip ${dst_file} ${lan_ip}
+        fi
         if [[ ${is_sign} == 1 ]]; then
             # 在<header之后插入sign.html整个文件
             sed -i -e '/<header/r '${sign_path}'/sign.html' ${dst_file}

src/blog-web/create-html.sh
@@ -33,7 +33,7 @@ copy_public_files() {
 
 init_begin
 create_sign ${src_path}/src/blog-web/sign.md ${tmp_html_path} ${is_public_ip} ${lan_ip}
-create_html ${src_path} ${tmp_html_path} ${sign_path} ${lan_ip}
+create_html ${src_path} ${tmp_html_path} ${sign_path} ${is_public_ip} ${lan_ip}
 copy_secret_repository
 copy_public_files
 change_perm ${tmp_html_path}