d631302b26295e5fc8c74d952d6c4e98bd652d9e
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-11-18 16:05:34 +0000

    push to gitee and github
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/push-git.sh
+++ src/blog-web/push-git.sh
@@ -1,6 +1,11 @@
 code_path=/home/sonvhi/chenxiaosong/code/
 github_io=chenxiaosonggithub.github.io
 
+git pull origin master
+git push origin master
+git pull github master
+git push github master
+
 bash ${code_path}/blog/src/blog-web/create-html.sh false this-arg-is-useless ${code_path}/${github_io}/
 cp ${code_path}/blog/src/blog-web/404.html ${code_path}/${github_io}/
 
