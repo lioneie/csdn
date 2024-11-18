1829c354dde3c66bcbb003eb0493790344f2ff8a
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-11-18 15:55:37 +0000

    update push-github-io.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/push-github-io.sh
+++ src/blog-web/push-github-io.sh
@@ -1,11 +1,12 @@
 code_path=/home/sonvhi/chenxiaosong/code/
+github_io=chenxiaosonggithub.github.io
 
-bash ${code_path}/blog/src/blog-web/create-html.sh false this-arg-is-useless ${code_path}/chenxiaosonggithub.github.io/
-cp ${code_path}/blog/src/blog-web/404.html ${code_path}/chenxiaosonggithub.github.io/
+bash ${code_path}/blog/src/blog-web/create-html.sh false this-arg-is-useless ${code_path}/${github_io}/
+cp ${code_path}/blog/src/blog-web/404.html ${code_path}/${github_io}/
 
-cd ${code_path}/chenxiaosonggithub.github.io/
+cd ${code_path}/${github_io}/
 git init
-git remote add origin git@github.com:chenxiaosonggithub/chenxiaosonggithub.github.io.git
+git remote add origin git@github.com:chenxiaosonggithub/${github_io}.git
 git add .
 git commit -s -m "chenxiaosong.com"
 git push origin master -f
