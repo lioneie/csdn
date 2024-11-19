1ee4d4c3efeae818baeebbf8059ecd3717e8498b
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-11-19 15:57:48 +0800

    rename to github-io-404.html and github-io-README.md
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/push-git.sh
+++ src/blog-web/push-git.sh
@@ -7,8 +7,8 @@ git pull github master
 git push github master
 
 bash ${code_path}/blog/src/blog-web/create-html.sh false this-arg-is-useless ${code_path}/${user_name}.github.io/
-cp ${code_path}/blog/src/blog-web/404-github-io.html ${code_path}/${user_name}.github.io/404.html
-cp ${code_path}/blog/src/blog-web/README-github-io.md ${code_path}/${user_name}.github.io/README.md
+cp ${code_path}/blog/src/blog-web/github-io-404.html ${code_path}/${user_name}.github.io/404.html
+cp ${code_path}/blog/src/blog-web/github-io-README.md ${code_path}/${user_name}.github.io/README.md
 echo "chenxiaosong.com" > ${code_path}/${user_name}.github.io/CNAME
 bash ${code_path}/blog/src/blog-web/github-io-create-index.sh ${code_path}/${user_name}.github.io/
 
