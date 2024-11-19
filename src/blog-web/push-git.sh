64e932b27713626cbb91c102fc003289e06366dc
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-11-19 10:27:22 +0800

    update push-git.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/push-git.sh
+++ src/blog-web/push-git.sh
@@ -1,19 +1,19 @@
 code_path=/home/sonvhi/chenxiaosong/code/
-github_io=chenxiaosonggithub.github.io
+user_name=chenxiaosonggithub
 
 git pull origin master
 git push origin master
 git pull github master
 git push github master
 
-bash ${code_path}/blog/src/blog-web/create-html.sh false this-arg-is-useless ${code_path}/${github_io}/
-cp ${code_path}/blog/src/blog-web/404.html ${code_path}/${github_io}/
-cp ${code_path}/blog/src/blog-web/README-github-io.md ${code_path}/${github_io}/README.md
+bash ${code_path}/blog/src/blog-web/create-html.sh false this-arg-is-useless ${code_path}/${user_name}.github.io/
+cp ${code_path}/blog/src/blog-web/404.html ${code_path}/${user_name}.github.io/
+cp ${code_path}/blog/src/blog-web/README-github-io.md ${code_path}/${user_name}.github.io/README.md
 
-cd ${code_path}/${github_io}/
+cd ${code_path}/${user_name}.github.io/
 echo "chenxiaosong.com" > CNAME
 git init
-git remote add origin git@github.com:chenxiaosonggithub/${github_io}.git
+git remote add origin git@github.com:${user_name}/${user_name}.github.io.git
 git add .
 git commit -s -m "chenxiaosong.com"
 git push origin master -f
