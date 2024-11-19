d43861de3c5aa782a322ba3a7f31161717a64aea
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-11-19 15:53:30 +0800

    github.io实现目录浏览功能
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/push-git.sh
+++ src/blog-web/push-git.sh
@@ -9,9 +9,10 @@ git push github master
 bash ${code_path}/blog/src/blog-web/create-html.sh false this-arg-is-useless ${code_path}/${user_name}.github.io/
 cp ${code_path}/blog/src/blog-web/404-github-io.html ${code_path}/${user_name}.github.io/404.html
 cp ${code_path}/blog/src/blog-web/README-github-io.md ${code_path}/${user_name}.github.io/README.md
+echo "chenxiaosong.com" > ${code_path}/${user_name}.github.io/CNAME
+bash ${code_path}/blog/src/blog-web/github-io-create-index.sh ${code_path}/${user_name}.github.io/
 
 cd ${code_path}/${user_name}.github.io/
-echo "chenxiaosong.com" > CNAME
 git init
 git remote add origin git@github.com:${user_name}/${user_name}.github.io.git
 git add .
