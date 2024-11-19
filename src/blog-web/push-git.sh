6158bb32caff4abe9726e701ed955d350956b750
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-11-19 12:12:06 +0800

    rename 404.html to 404-github-io.html
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/push-git.sh
+++ src/blog-web/push-git.sh
@@ -7,7 +7,7 @@ git pull github master
 git push github master
 
 bash ${code_path}/blog/src/blog-web/create-html.sh false this-arg-is-useless ${code_path}/${user_name}.github.io/
-cp ${code_path}/blog/src/blog-web/404.html ${code_path}/${user_name}.github.io/
+cp ${code_path}/blog/src/blog-web/404-github-io.html ${code_path}/${user_name}.github.io/404.html
 cp ${code_path}/blog/src/blog-web/README-github-io.md ${code_path}/${user_name}.github.io/README.md
 
 cd ${code_path}/${user_name}.github.io/
