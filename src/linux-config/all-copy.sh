b1b26c643de62b5cb3e246b5c34190efd4623dee
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-11-27 10:26:03 +0800

    add top-path config file
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/linux-config/all-copy.sh
+++ src/linux-config/all-copy.sh
@@ -1,3 +1,3 @@
 cmd="cp"
-array=(gitconfig set_proxy.sh origin_xmodmap.txt xmodmap.txt vimrc emacs bash_profile tmux.conf)
+array=(gitconfig set_proxy.sh origin_xmodmap.txt xmodmap.txt vimrc emacs bash_profile tmux.conf top-path)
 . common.sh
--- src/linux-config/link.sh
+++ src/linux-config/link.sh
@@ -1,3 +1,3 @@
 cmd="ln -s"
-array=(origin_xmodmap.txt xmodmap.txt vimrc emacs bash_profile tmux.conf)
+array=(origin_xmodmap.txt xmodmap.txt vimrc emacs bash_profile tmux.conf top-path)
 . common.sh
