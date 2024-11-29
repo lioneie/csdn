72e2c021b24edcf82e4cdc65b9f0facda67b3905
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-11-29 12:36:35 +0800

    update restart.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/restart.sh
+++ src/blog-web/restart.sh
@@ -10,19 +10,21 @@ is_restart=false # 是否重新启动
 # 更新git仓库代码
 # $1: 仓库名， $2: 是否推送到github
 update_repo() {
+    local repo=$1
+    local is_github=$2
     # 如果是局域网ip，就不更新仓库
     if [ ${is_replace_ip} = true ]; then
         is_restart=true # 但要重启所有
         return
     fi
-    cd ${code_path}/${1}/
+    cd ${code_path}/${repo}/
     timeout 20 git fetch origin # 最多20秒超时，有时会因为网络原因卡住
     local_head=$(git rev-parse HEAD)
     origin_head=$(git rev-parse origin/master)
     if [ "${local_head}" != "${origin_head}" ]; then
         timeout 20 git pull origin master
         is_restart=true
-        if [ ${2} = true ]; then
+        if [ ${is_github} = true ]; then
             timeout 20 git push github master
         fi
     fi
@@ -33,7 +35,7 @@ update_repo blog ${is_replace_ip} # 部署在公网服务器就推到github
 update_repo tmp false # 不用推到github
 update_repo private-blog false # 不用推到github
 
-if [ ${is_restart} = false ]; then
+if [ "${is_restart}" = false ]; then
     echo "no change"
 fi
 bash ${code_path}/blog/src/blog-web/do-restart.sh ${is_replace_ip} ${other_ip} ${is_restart}
