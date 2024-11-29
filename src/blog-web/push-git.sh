714857f0267331ae6aa75293ba887012c724c4c1
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-11-29 11:20:53 +0800

    generate_index()移动到common-lib.sh中
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/push-git.sh
+++ src/blog-web/push-git.sh
@@ -1,6 +1,8 @@
 . ~/.top-path
 code_path=${MY_CODE_TOP_PATH}
 user_name=chenxiaosonggithub
+# 导入其他脚本
+. ${code_path}/blog/src/blog-web/common-lib.sh
 
 git pull origin master
 git push origin master
@@ -11,7 +13,7 @@ bash ${code_path}/blog/src/blog-web/create-html.sh false this-arg-is-useless ${c
 cp ${code_path}/blog/src/blog-web/github-io-404.html ${code_path}/${user_name}.github.io/404.html
 cp ${code_path}/blog/src/blog-web/github-io-README.md ${code_path}/${user_name}.github.io/README.md
 echo "chenxiaosong.com" > ${code_path}/${user_name}.github.io/CNAME
-bash ${code_path}/blog/src/blog-web/github-io-create-index.sh ${code_path}/${user_name}.github.io/
+generate_index ${code_path}/${user_name}.github.io/
 
 cd ${code_path}/${user_name}.github.io/
 git init
