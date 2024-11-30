e3f52f386c8547cddb88f7cdae5f17084c84a54f
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2024-11-30 13:21:35 +0000

    update check-git.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/check-git.sh
+++ src/blog-web/check-git.sh
@@ -1,68 +1,31 @@
 . ~/.top-path
 code_path=${MY_CODE_TOP_PATH}
-DEBUG=1
+MY_ECHO_DEBUG=0
+
+# 导入其他脚本
+. ${code_path}/blog/src/blog-web/common-lib.sh
 
 not_exist_repos=()
 not_clean_repos=()
 not_sync_repos=()
 ok_repos=()
 
-my_echo() {
-	if [ "$DEBUG" -eq 1 ]; then
-		echo "$@"
-	fi
+print_result() {
+	echo
+	echo "未提交的仓库: ${not_clean_repos[@]}"
+	echo "未push/pull的仓库: ${not_sync_repos[@]}"
+	echo "不存在的仓库: ${not_exist_repos[@]}"
+	echo "全部搞定的仓库: ${ok_repos[@]}"
 }
 
 check_git() {
 	local repo=$1
-	local path=${code_path}/${repo}
-
-	if [ ! -d "${path}" ]; then
-		my_echo "${repo}目录不存在"
-		not_exist_repos+=(${repo})
-		return
-	fi
-
-	cd ${path}
-	status=$(git status -s)
-
-	if [ ! -z "${status}" ]; then
-		my_echo "${repo}有未提交的更改:"
-		my_echo "${status}"
-		not_clean_repos+=(${repo})
-		return
-	fi
-
-	git fetch origin
-	if [ $? -ne 0 ]; then
-		echo "!!! ${repo} fetch fail !!!"
-		exit
-	fi
-	origin_commit=$(git rev-parse origin/master)
-	master_commit=$(git rev-parse master)
-	my_echo "${repo} origin_commit: ${origin_commit}"
-	my_echo "${repo} master_commit: ${master_commit}"
-
-	if [ "${origin_commit}" == "${master_commit}" ]; then
-		my_echo "${repo}全部搞定"
-		ok_repos+=(${repo})
-	else
-		my_echo "${repo}未push/pull"
-		not_sync_repos+=(${repo})
-	fi
-}
-
-print_result() {
-	my_echo
-	my_echo "未提交的仓库: ${not_clean_repos[@]}"
-	my_echo "未push/pull的仓库: ${not_sync_repos[@]}"
-	my_echo "不存在的仓库: ${not_exist_repos[@]}"
-	my_echo "全部搞定的仓库: ${ok_repos[@]}"
+	check_repo ${code_path}/${repo} not_exist_repos not_clean_repos not_sync_repos ok_repos
 }
 
 check_git "blog"
 check_git "tmp"
 check_git "private-blog"
 check_git "private-tmp"
-
 print_result
+. ${code_path}/private-blog/scripts/check-git.sh
