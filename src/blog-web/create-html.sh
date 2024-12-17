lioneie/csdn

commit 14cfdf57f53ef161297a6df118145c0d22590313

update create-html.sh
Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

src/blog-web/create-html.sh
@@ -18,6 +18,8 @@ fi
 . ${src_path}/src/blog-web/array.sh
 
 my_init() {
+	rm -rf ${html_path}
+	mkdir -p ${html_path}
 	rm -rf ${tmp_html_path}
 	mkdir -p ${tmp_html_path}
 	rm -rf ${tmp_src_path}

commit 2a1918afbc498f7a281170e92fb025a5af85bddd

修复nginx正在生成html时暂时不能访问的问题
Fixes: 14cfdf5 ("update create-html.sh")
Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

src/blog-web/create-html.sh
@@ -18,7 +18,6 @@ fi
 . ${src_path}/src/blog-web/array.sh
 
 my_init() {
-	rm -rf ${html_path}
 	mkdir -p ${html_path}
 	rm -rf ${tmp_html_path}
 	mkdir -p ${tmp_html_path}