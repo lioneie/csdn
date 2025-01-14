585bd51890b243476025678bb68d1ebc416bee68
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-01-14 22:41:41 +0800

    create cve html
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/array.sh
+++ src/blog-web/array.sh
@@ -151,6 +151,9 @@ comm_array=(
 	1 1 src/filesystem/tmpfs-oom.md ~ "tmpfs不断写导致oom的问题"
 	1 1 src/filesystem/lazy-umount.md ~ "文件系统延迟卸载"
 	# cve
+	1 1 src/cve/nfs-cve.md ~ "nfs cve"
+	1 1 src/cve/smb-cve.md ~ "smb cve"
+	1 1 src/cve/others-cve.md ~ "未分类的cve"
 	# Linux环境
 	1 1 src/userspace-environment/vnc.md ~ "VNC远程桌面"
 	1 1 src/blog-web/blog-web.md ~ "如何拥有个人域名的网站和邮箱"
