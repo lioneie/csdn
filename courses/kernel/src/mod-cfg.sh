4c34ce3e86be0013c9c2413e7fa955874b135825
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-01-14 10:21:09 +0800

    update mod-cfg.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- courses/kernel/src/mod-cfg.sh
+++ courses/kernel/src/mod-cfg.sh
@@ -3,16 +3,18 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
+build_dir=x86_64-build
 mnt_point=/tmp/9p
 knl_base_dir=${mnt_point}/code/$1 # 修改成内核代码目录
 mkdir $mnt_point
 mkdir /lib/modules -p
 mount -t 9p -o trans=virtio 9p $mnt_point
 knl_vers=$(uname -r)
-target=${knl_base_dir}/build/mod/lib/modules/${knl_vers}
+target=${knl_base_dir}/${build_dir}/mod/lib/modules/${knl_vers}
 link_name=/lib/modules/${knl_vers}
 rm ${link_name} -rf
 ln -s ${target} ${link_name}
 # 重新链接build目录
 rm /lib/modules/${knl_vers}/build
-ln -s ${knl_base_dir}/build/ /lib/modules/${knl_vers}/build
+ln -s ${knl_base_dir}/${build_dir}/ /lib/modules/${knl_vers}/build
+
