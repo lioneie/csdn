40b415ef0e41c229de79efcab845351fc97edca6
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-02-12 15:24:43 +0800

    create 4.19-nfs-soft-lockup-in-__rpc_execute.html
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/blog-web/array.sh
+++ src/blog-web/array.sh
@@ -106,6 +106,7 @@ comm_array=(
 			1 1 courses/nfs/issues/nfsiostat-queue-long-time.md ~ "nfsiostat命令queue时间长的问题"
 			1 1 courses/nfs/issues/null-ptr-deref-in-nfs_ctx_key_to_expire.md ~ "nfs_ctx_key_to_expire()引用计数泄露和空指针解引用的问题"
 			1 1 courses/nfs/issues/cthon-nfs-tests.md ~ "Connectathon NFS tests测试问题"
+			1 1 courses/nfs/issues/4.19-nfs-soft-lockup-in-__rpc_execute.md ~ "4.19 __rpc_execute() soft lockup的问题"
 	0 1 courses/smb/smb.md ~ "smb文件系统"
 		1 1 courses/smb/video.md ~ "smb课程配套视频"
 		1 1 courses/smb/smb-introduction.md ~ "smb简介"
