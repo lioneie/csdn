1ed8600cb86e48476238b3fbf0de7b2ae47c2ad8
ChenXiaoSong <chenxiaosong@chenxiaosong.com>
2025-02-21 09:57:42 +0800

    update grep.sh
    Signed-off-by: ChenXiaoSong <chenxiaosong@chenxiaosong.com>

--- src/scripts/grep.sh
+++ src/scripts/grep.sh
@@ -1,3 +1,10 @@
+. ~/.top-path
+code_path=${MY_CODE_TOP_PATH}
+MY_ECHO_DEBUG=0
+
+# 导入其他脚本
+. ${code_path}/blog/src/blog-web/common-lib.sh
+
 # 检查参数
 if [ $# -ne 1 ]; then
         echo "用法: bash $0 <要查找的字符串>"
@@ -5,11 +12,27 @@ if [ $# -ne 1 ]; then
 fi
 string=$1
 
-while IFS= read -r file; do
-	if [[ "${file}" == "." || "${file}" == ".." || \
-	      "${file}" == ".git" ]]; then
-		continue
+grep_repo() {
+	local repo=$1
+	local repo_path="${code_path}/${repo}"
+
+	if [ ! -d "${repo_path}" ]; then
+		return
 	fi
-	grep -r "${string}" "${file}" # 也可以直接用 | grep -v "^\.git/"
-done < <(printf "%s\n" "$(ls -1 -a)")
+	cd "${repo_path}"
+
+	while IFS= read -r file; do
+		if [[ "${file}" == "." || "${file}" == ".." || \
+		      "${file}" == ".git" ]]; then
+			continue
+		fi
+		grep -rHnT "${string}" "${file}" # 也可以直接用 | grep -v "^\.git/"
+	done < <(printf "%s\n" "$(ls -1 -a)")
+}
+
+grep_repo "blog"
+grep_repo "tmp"
+grep_repo "private-blog"
+grep_repo "private-tmp"
+grep_repo "myfs"
 
